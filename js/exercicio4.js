24fc7bd4302439bbd5fea6fc3ef0f544e4bd33d3
Hernando Silva <hernando_silva2004@outlook.com>
2024-05-03 21:20:06 -0300

    exercicio 4 arrumado, finalizado

--- js/exercicio4.js
+++ js/exercicio4.js
@@ -1,21 +1,27 @@
 function executarExercicio4() {
+    
+    let temp = 0
+    
+    let maior1 = parseInt(prompt(`Informe o 1º número: `))
 
-    let valor = parseInt(prompt(`Informe o 1º número: `))
-    let maior1 = valor
-    let maior2 = valor
-
-    for (let contadora = 2; contadora <= 3; contadora++) {
-        valor = parseInt(prompt(`Informe o ${contadora}º número: `))
-
-        if (valor > maior1) {
-            maior1 = valor
-        } else if (valor > maior2){
-            maior2 = valor
-        }
-        
-
+    let maior2 = parseInt(prompt(`Informe o 2º número: `))
+    
+    if (maior2 > maior1) {
+        temp = maior1
+        maior1 = maior2
+        maior2 = temp
     }
-    let soma = maior1 + maior2;
+    
+    let valor = parseInt(prompt(`Informe o 3º número: `))
 
-    alert(`A soma dos dois maiores valores é: ${soma}. E eles são: ${maior1} e ${maior2}`)
+    if(valor>maior1){
+        maior2 = maior1
+        maior1 = valor
+    } else if(valor> maior2){
+        maior2 = valor
+    }
+
+    soma = maior1 + maior2;
+
+alert(`A soma dos dois maiores valores é: ${soma}. E eles são: ${maior1} e ${maior2}`)
 }
